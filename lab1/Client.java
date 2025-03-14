35c78dbb1eabf2395c36f06cb57528eb6a8db8d3
asmaalrifai <115134613+asmaalrifai@users.noreply.github.com>
2025-03-14 16:48:36 +0300

    Create Client.java

---
+++ lab1/Client.java
@@ -0,0 +1,30 @@
+package AsmaAlrefaei2221251369;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author asma
+ */
+public class Client {
+    Socket cSocket;
+    public void connect(String ip, int port){
+        try {
+            cSocket = new Socket(ip,port);
+            OutputStream cOut = cSocket.getOutputStream();                
+            cOut.write(" message".getBytes());
+            
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void main(String[] args) {
+        Client c1 = new Client();
+        c1.connect("172.31.1.47", 5000);  
+    }
+}
